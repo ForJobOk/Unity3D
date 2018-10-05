c7e1b9dae3e974ceca94ba32d75c9f3b0c5b51c8
ForJobOk <41860455+ForJobOk@users.noreply.github.com>
2018-10-05 18:12:03 +0900

    Update GravityController.cs
    学習内容:①エディター確認_②加速度センサー_③Normalize,normalize
    ①ApplicationクラスのisEditorでエディター上でプレイしているか確認できる。これにif文を組み合わせて、PC上では不可能な操作はスキップできる。
    ②Input.acceleration.方向;　で指定した方向の加速度センサーの反応を取得できる。
    ③Normalize、normalizeそれぞれでベクトルを正規化（１に）できる。微妙に違うらしいが説明見てもよくわからん。

--- GravityController.cs
+++ GravityController.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+//学習内容:①エディター確認_②加速度センサー_③Normalize,normalize
+//①ApplicationクラスのisEditorでエディター上でプレイしているか確認できる。これにif文を組み合わせて、PC上では不可能な操作はスキップできる。
+//②Input.acceleration.方向;　で指定した方向の加速度センサーの反応を取得できる。
+//③Normalize、normalizeそれぞれでベクトルを正規化（１に）できる。微妙に違うらしいが説明見てもよくわからん。
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
